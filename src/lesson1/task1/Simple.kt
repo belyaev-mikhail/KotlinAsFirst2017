b71c13e1bba67934656e74ce24954b85e447050c
Mikhail Belyaev <pindragon@yandex.ru>
2017-09-11 01:15:11 +0300

    aaa

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -60,7 +60,8 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = seconds + 60 * (minut
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = (sagenes + arshins + vershoks).toDouble()
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double =
+        (vershoks + sagenes * 48 + arshins * 16) * 4.445
 
 /**
  * Тривиальная
@@ -68,7 +69,8 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = (sagenes
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(grad: Int, min: Int, sec: Int): Double = TODO()
+fun angleInRadian(grad: Int, min: Int, sec: Int): Double =
+        (sec + 60 * min + 60 * 60 * grad) * Math.PI / 43200
 
 /**
  * Тривиальная
@@ -76,7 +78,8 @@ fun angleInRadian(grad: Int, min: Int, sec: Int): Double = TODO()
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double =
+        Math.sqrt((x1 - x2).let{ it * it } + (y1 - y2).let{ it * it })
 
 /**
  * Простая
@@ -84,7 +87,7 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = TODO()
+fun thirdDigit(number: Int): Int = numberRevert(number) % 10
 
 /**
  * Простая
@@ -110,4 +113,4 @@ fun accountInThreeYears(initial: Int, percent: Int): Double = (1.0 + percent / 1
  * Пользователь задает целое трехзначное число (например, 478).
  *Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = TODO()
+fun numberRevert(number: Int): Int = number.toString().reverse().toInt()
