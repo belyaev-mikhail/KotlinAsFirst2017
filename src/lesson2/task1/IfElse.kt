9442adf7baeabc148e02835b613be057a1516525
Mikhail Belyaev <pindragon@yandex.ru>
2017-10-01 01:33:12 +0300

    Update IfElse.kt

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -33,7 +33,7 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String = "$age лет"
+fun ageDescription(age: Int): String = "$age годиков"
 
 /**
  * Простая
