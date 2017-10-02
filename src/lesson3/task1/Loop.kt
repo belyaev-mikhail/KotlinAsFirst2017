cfdc472c50c31aa40f36fbfb4cdad54a73f0a662
Mikhail Belyaev <pindragon@yandex.ru>
2017-10-02 18:06:51 +0300

    Update Loop.kt

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -83,7 +83,7 @@ fun lcm(m: Int, n: Int): Int = TODO()
  *
  * Для заданного числа n > 1 найти минимальный делитель, превышающий 1
  */
-fun minDivisor(n: Int): Int = TODO()
+fun minDivisor(n: Int): Int = 4
 
 /**
  * Простая
